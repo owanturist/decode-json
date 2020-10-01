efd1eacf4653c12f056e1030958d203718d0bb65
Anton Ovechkin <owanturist@gmail.com>
2020-10-01 19:47:44 +0200

    [enhancement/5] Test map and chain

---
+++ tests/chain.test.ts
@@ -0,0 +1,27 @@
+import test from 'ava'
+
+import Decode from '../src'
+import { RequiredField } from '../src/error'
+
+test('Decoder.chain()', t => {
+  // Decoder<string>
+  const _0 = Decode.field('active').boolean.chain(active => {
+    return active
+      ? Decode.field('start_date').string
+      : Decode.field('last_activity').string
+  })
+
+  t.is(
+    _0.decode({ active: false, last_activity: '30 sec ago' }).value,
+    '30 sec ago'
+  )
+  t.is(
+    _0.decode({ active: true, start_date: '1 Sep 2020' }).value,
+    '1 Sep 2020'
+  )
+
+  t.deepEqual(
+    _0.decode({ active: true, last_activity: '30 sec ago' }).error,
+    RequiredField('start_date', { active: true, last_activity: '30 sec ago' })
+  )
+})
